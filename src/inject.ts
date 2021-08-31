94df8bbb32f4ce403b21728a3b18415d177c6391
anzerr <thoron174+anzerr@gmail.com>
2021-08-31 18:03:09 +0200

    🚧 linter fix

--- src/inject.ts
+++ src/inject.ts
@@ -7,12 +7,12 @@ const Inject = (dep: (() => Record<string, any>) | Record<string, any>) => {
 	return (target: any, key: string, index?: number) => {
 		if (index !== undefined) {
 			const original = Reflect.getMetadata(METADATA.DEPENDANCYPARAM, target) || [];
-			const out = Util.copy<any>(original)
+			const out = Util.copy<any>(original);
 			out.push({dep: dep, index: index});
 			Reflect.defineMetadata(METADATA.DEPENDANCYPARAM, out, target);
 		} else {
 			const original = Reflect.getMetadata(METADATA.DEPENDANCY, target.constructor) || [];
-			const out = Util.copy<any>(original)
+			const out = Util.copy<any>(original);
 			out.push({dep: dep, key: key});
 			Reflect.defineMetadata(METADATA.DEPENDANCY, out, target.constructor);
 		}
